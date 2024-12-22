b74c65d16800250c88508c1c767d554527ae5d0d
mani <manirazeghi049@gmail.com>
2024-12-22 00:15:46 -0800

    possible_strin_self

--- possible.py
+++ possible.py
@@ -1,3 +1,8 @@
 
 def possible_list(number: int, data: list[int]) -> float:
     return f'chance: {data.count(number) / len(data) * 100:.1f} %'
+
+
+def possible_string_self(letter: str, word: str) -> float:
+    return f'chance: {word.count(letter) / len(word) * 100:.1f} %'
+
